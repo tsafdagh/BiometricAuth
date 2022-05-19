ee71e23cc8b312d27647cd0435db905a8a0b0b86
tsafdagh <tsafackdcedrick@gmail.com>
2022-05-19 23:11:17 +0100

    add shape of card background

---
+++ app/src/main/java/com/biometric/biometricauth/testComponent.kt
@@ -0,0 +1,104 @@
+package com.biometric.biometricauth
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.GenericShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.intl.Locale
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+
+@Composable
+@Preview
+fun PreviewCard() {
+    BuildCard()
+}
+
+@Composable
+fun BuildCard(modifier: Modifier = Modifier) {
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(250.dp)
+    ) {
+
+        /*     val triangleShape = GenericShape { size, _ ->
+                 moveTo(0f, 0f)
+                 lineTo(size.width, 0f )
+
+                 lineTo(size.width / 1f, size.height / 1f)
+             }*/
+
+        val triangleShape = GenericShape { size, _ ->
+            moveTo(size.width, 0f)
+
+            lineTo(size.width, size.height)
+            lineTo(0f, size.height)
+        }
+
+        Box(
+            modifier = Modifier
+                .fillMaxHeight(1f)
+                .fillMaxWidth(1f)
+                .shadow(10.dp, shape = RoundedCornerShape(CornerSize(10.dp)))
+                .background(
+                    color = colorResource(id = R.color.teal_200),
+                    shape = RoundedCornerShape(CornerSize(10.dp))
+                )
+                .padding(0.dp)
+        ) {
+
+            Surface(
+                modifier =
+                Modifier.fillMaxSize(),
+                elevation = 2.dp,
+                shape = triangleShape,
+                color = colorResource(id = R.color.teal_200_low)
+            ) {
+            }
+            val color = colorResource(id = R.color.teal_200_low_center)
+            Canvas(
+                modifier = Modifier.fillMaxSize()
+            ) {
+
+                // Fetching width and height for
+                // setting start x and end y
+                val canvasWidth = size.width
+                val canvasHeight = size.height
+
+                // drawing a line between start(x,y) and end(x,y)
+                drawLine(
+                    start = Offset(x = canvasWidth, y = 0f),
+                    end = Offset(x = 0f, y = canvasHeight),
+                    color = color,
+                    strokeWidth = 9F
+                )
+            }
+
+
+        }
+    }
+
+}
